cd7909f9a37af3a9d282554fee3f9c10b780e9f9
Veman Jadhav <vemancraft@gmail.com>
2023-10-23 23:08:26 +0530

    Day 4 - The pain has begun.
    Trying to add pathfinding, so I thought of making a level in the project and exporting it for adding navmeshes.
    
    Coming across a "geometry.addAttribute" error when trying to download the said GLTF file.

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 import * as THREE from "three";
-import Ammo from "./Scripts/Addons/ammo";
+import OBJExporter from "three-obj-exporter";
+// import OBJExporter from "./Scripts/Addons/OBJExporter";
+import _GLTFExporter from "three-gltf-exporter";
 import Player from "./Scripts/Player";
 import IsoCamera from "./Scripts/IsoCamera";
 
 //Initialization of variables and objects.
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
-const geometry = new THREE.BoxGeometry(5, 1, 5);
+const geometry = new THREE.BoxGeometry(7.5, 1, 7.5);
 const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const platform = new THREE.Mesh(geometry, material);
 const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
+// const exporter = new _GLTFExporter();
+const btn = document.getElementById("download-glb");
 // const helper = new THREE.CameraHelper(dirLight.shadow.camera);
 var player = new Player();
 var mainCamera = new IsoCamera(75, 1, 0.1, 1000);
-
-const collisionConfig = new Ammo.btDefaultCollisionConfiguration();
 
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -43,11 +45,12 @@ player.position.set(0, 1, 0);
 mainCamera.position.set(-5, 5, 5);
 mainCamera.lookAt(player.position);                 //Calling this on start so that the camera is always looking at the player.
 
-renderer.setSize(725, 725);
+renderer.setSize(600, 600); // renderer.setSize(725, 725);
 document.body.appendChild(renderer.domElement);
 // document.addEventListener("keydown", player.MovePlayer.bind(player), false);
 document.addEventListener("pointermove", onPointerMove);
 document.addEventListener("mousedown", onMouseDown);
+btn.addEventListener("click", download);
 
 function onPointerMove(event)
 {
@@ -78,3 +81,23 @@ function animate()
 }
 
 animate();
+
+function download()
+{
+    const exporter = new _GLTFExporter();
+    exporter.parse(scene, (result) => { saveArrayBuffer(result, "testScene.glb"); }, { binary: true });
+}
+
+function saveArrayBuffer(buffer, fileName)
+{
+    save(new Blob([buffer], {type: "application/octet-stream"}), fileName);
+}
+
+function save(_blob, fileName)
+{
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    link.href = URL.createObjectURL(_blob);
+    link.dowload = fileName;
+    link.click();
+}
